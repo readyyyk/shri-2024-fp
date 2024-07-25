6263000db6ab342c3c17dc147c23d1c5b3bc1876
readyyyk <mr.sairexer@gmail.com>
2024-07-26 00:29:05 +0300

    [TODO: handle api error]; added: task 2

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,88 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+    curry,
+    __,
+    compose,
+    tap,
+    applyTo,
+    length,
+    allPass,
+    lt,
+    gt,
+    test,
+    ifElse,
+    not,
+    ap,
+    modify, andThen, modulo, partialRight,
+} from 'ramda';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isPromise = (val) => val && typeof val.then === 'function';
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const getProp = curry((obj, prop) => obj[prop]);
+const getValue = getProp(__, 'value');
+const getWriteLog = getProp(__, 'writeLog');
+const getHandleSuccess = getProp(__, 'handleSuccess');
+const getHandleError = getProp(__, 'handleError');
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const apiDecToBin = async (dec) =>
+    api.get('https://api.tech/numbers/base', {from: 10, to: 2, number: String(dec)})
+        .then(a => a.result);
+const apiGetAnimal = async (num) =>
+    api.get('https://animals.tech/'+num, {})
+        .then(a => a.result)
+        .catch(e=>console.error(e));
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const applyToMaybePromise = ([a, b]) =>
+    isPromise(a)
+        ? applyTo(a, andThen(b))
+        : applyTo(a, b)
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const tappedHandleValue = handler => tap(compose(
+    applyToMaybePromise,
+    tap(console.log),
+    ap([getValue, handler]),
+    a => [a],
+));
+const tappedWriteValueLog = tappedHandleValue(getWriteLog);
+const tappedHandleValueSuccess = tappedHandleValue(getHandleSuccess);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const validate = allPass([
+    compose(tap(console.log), test(/^[0-9]*\.?[0-9]+$/g)),
+    compose(gt(__, 2), length),
+    compose(lt(__, 10), length)
+]);
+const tappedValidate = tap(
+    ifElse(
+        compose(not, validate, getValue),
+        compose(a=>a('ValidationError'), getHandleError),
+        // compose('ValidationError', apply, getHandleError), // THROWS "g.call is not a function"
+        ()=>{},
+    )
+)
+
+const pow2 = partialRight(Math.pow, [2]);
+const modulo3 = modulo(__, 3);
+const processSequence = compose(
+    tappedHandleValueSuccess,
+    modify('value', andThen(apiGetAnimal)),
+    tappedWriteValueLog,
+    modify('value', andThen(modulo3)),
+    tappedWriteValueLog,
+    modify('value', andThen(pow2)),
+    tappedWriteValueLog,
+    modify('value', andThen(length)),
+    tappedWriteValueLog,
+    modify('value', apiDecToBin),
+    tappedWriteValueLog,
+    modify('value', Math.round),
+    modify('value', parseFloat),
+    tappedValidate,
+    tappedWriteValueLog,
+);
 
 export default processSequence;
